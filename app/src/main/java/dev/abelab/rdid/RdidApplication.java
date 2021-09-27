8ea057c0d1b686dd1cc146db21efd966ffed2a93
averak <abe12@mccc.jp>
2021-09-27 12:36:38 +0900

    [feat] add spring-boot entry point

---
+++ app/src/main/java/dev/abelab/rdid/RdidApplication.java
@@ -0,0 +1,13 @@
+package dev.abelab.rdid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdidApplication.class, args);
+    }
+
+}
