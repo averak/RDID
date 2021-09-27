868b42e1710603337500ba174ab64b7c69730146
averak <abe12@mccc.jp>
2021-09-27 14:23:08 +0900

    [test] user logic unit test

---
+++ app/src/test/java/dev/abelab/rdid/logic/AbstractLogic_UT.java
@@ -0,0 +1,14 @@
+package dev.abelab.rdid.logic;
+
+import dev.abelab.rdid.annotation.UnitTest;
+
+/**
+ * Abstract Logic Unit Test
+ */
+@UnitTest
+public abstract class AbstractLogic_UT {
+
+	static final Integer SAMPLE_INT = 1;
+	static final String SAMPLE_STR = "SAMPLE";
+
+}
