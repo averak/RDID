fd4a6aa60185f28d4bcfc7d7397e33006cf957ad
averak <abe12@mccc.jp>
2021-09-27 13:46:19 +0900

    [feat] add mybatis custom mappers

---
+++ app/src/main/java/dev/abelab/rdid/db/mapper/UserGroupMemberMapper.java
@@ -0,0 +1,6 @@
+package dev.abelab.rdid.db.mapper;
+
+import dev.abelab.rdid.db.mapper.base.UserGroupMemberBaseMapper;
+
+public interface UserGroupMemberMapper extends UserGroupMemberBaseMapper {
+}
---
+++ app/src/main/java/dev/abelab/rdid/db/mapper/UserMapper.java
@@ -0,0 +1,6 @@
+package dev.abelab.rdid.db.mapper;
+
+import dev.abelab.rdid.db.mapper.base.UserBaseMapper;
+
+public interface UserMapper extends UserBaseMapper {
+}
