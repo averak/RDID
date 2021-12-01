c92023056b698e3401dddb18f06fbde2dca49dce
averak <abe12@mccc.jp>
2021-12-01 15:21:55 +0900

    [test] user detele api integration test

--- app/src/test/java/dev/abelab/rdid/api/controller/UserRestController_IT.java
+++ app/src/test/java/dev/abelab/rdid/api/controller/UserRestController_IT.java
@@ -47,6 +47,7 @@ public class UserRestController_IT extends AbstractRestController_IT {
 	static final String GET_USERS_PATH = BASE_PATH;
 	static final String CREATE_USER_PATH = BASE_PATH;
 	static final String UPDATE_USER_PATH = BASE_PATH + "/%d";
+	static final String DELETE_USER_PATH = BASE_PATH + "/%d";
 
 	@Autowired
 	ModelMapper modelMapper;
@@ -328,6 +329,60 @@ public class UserRestController_IT extends AbstractRestController_IT {
 			 * test & verify
 			 */
 			final var request = putRequest(String.format(UPDATE_USER_PATH, SAMPLE_INT), requestBody);
+			request.header(HttpHeaders.AUTHORIZATION, "");
+			execute(request, new UnauthorizedException(ErrorCode.INVALID_ACCESS_TOKEN));
+		}
+
+	}
+
+	/**
+	 * ユーザ削除APIのIT
+	 */
+	@Nested
+	@TestInstance(PER_CLASS)
+	class DeleteUser_IT extends AbstractRestControllerInitialization_IT {
+
+		@Test
+		void 正_ユーザを削除() throws Exception {
+			/*
+			 * given
+			 */
+			final var loginUser = createLoginUser();
+			final var credentials = getLoginUserCredentials(loginUser);
+
+			final var user = UserSample.builder().build();
+			userMapper.insert(user);
+
+			/*
+			 * test & verify
+			 */
+			final var request = deleteRequest(String.format(DELETE_USER_PATH, user.getId()));
+			request.header(HttpHeaders.AUTHORIZATION, credentials);
+			execute(request, HttpStatus.OK);
+		}
+
+		@Test
+		void 異_削除対象ユーザが存在しない() throws Exception {
+			/*
+			 * given
+			 */
+			final var loginUser = createLoginUser();
+			final var credentials = getLoginUserCredentials(loginUser);
+
+			/*
+			 * test & verify
+			 */
+			final var request = deleteRequest(String.format(DELETE_USER_PATH, SAMPLE_INT));
+			request.header(HttpHeaders.AUTHORIZATION, credentials);
+			execute(request, new NotFoundException(ErrorCode.NOT_FOUND_USER));
+		}
+
+		@Test
+		void 異_無効な認証ヘッダ() throws Exception {
+			/*
+			 * test & verify
+			 */
+			final var request = deleteRequest(String.format(DELETE_USER_PATH, SAMPLE_INT));
 			request.header(HttpHeaders.AUTHORIZATION, "");
 			execute(request, new UnauthorizedException(ErrorCode.INVALID_ACCESS_TOKEN));
 		}
