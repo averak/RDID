20d03866902a7d265933e8ee63ce0cb03ec3779b
averak <abe12@mccc.jp>
2021-12-01 12:59:18 +0900

    [test] create user api integration test

--- app/src/test/java/dev/abelab/rdid/api/controller/UserRestController_IT.java
+++ app/src/test/java/dev/abelab/rdid/api/controller/UserRestController_IT.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.*;
 import static org.junit.jupiter.params.provider.Arguments.*;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.stream.Stream;
 import java.util.stream.Collectors;
 
@@ -19,15 +20,19 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.modelmapper.ModelMapper;
-import mockit.Expectations;
-import mockit.Mocked;
 
+import dev.abelab.rdid.api.request.UserCreateRequest;
 import dev.abelab.rdid.api.response.UserResponse;
 import dev.abelab.rdid.api.response.UsersResponse;
+import dev.abelab.rdid.db.entity.User;
+import dev.abelab.rdid.db.entity.UserExample;
 import dev.abelab.rdid.db.mapper.UserMapper;
 import dev.abelab.rdid.helper.sample.UserSample;
 import dev.abelab.rdid.helper.util.RandomUtil;
 import dev.abelab.rdid.exception.ErrorCode;
+import dev.abelab.rdid.exception.BaseException;
+import dev.abelab.rdid.exception.BadRequestException;
+import dev.abelab.rdid.exception.ConflictException;
 import dev.abelab.rdid.exception.UnauthorizedException;
 
 /**
@@ -38,6 +43,7 @@ public class UserRestController_IT extends AbstractRestController_IT {
 	// API PATH
 	static final String BASE_PATH = "/api/users";
 	static final String GET_USERS_PATH = BASE_PATH;
+	static final String CREATE_USER_PATH = BASE_PATH;
 
 	@Autowired
 	ModelMapper modelMapper;
@@ -95,6 +101,130 @@ public class UserRestController_IT extends AbstractRestController_IT {
 			 * test & verify
 			 */
 			final var request = getRequest(GET_USERS_PATH);
+			request.header(HttpHeaders.AUTHORIZATION, "");
+			execute(request, new UnauthorizedException(ErrorCode.INVALID_ACCESS_TOKEN));
+		}
+
+	}
+
+	/**
+	 * ユーザ作成APIのIT
+	 */
+	@Nested
+	@TestInstance(PER_CLASS)
+	class CreateUser_IT extends AbstractRestControllerInitialization_IT {
+
+		@Test
+		void ユーザを作成() throws Exception {
+			/*
+			 * given
+			 */
+			final var loginUser = createLoginUser();
+			final var credentials = getLoginUserCredentials(loginUser);
+
+			final var user = UserSample.builder().password(LOGIN_USER_PASSWORD).build();
+			final var requestBody = modelMapper.map(user, UserCreateRequest.class);
+
+			/*
+			 * test
+			 */
+			final var request = postRequest(CREATE_USER_PATH, requestBody);
+			request.header(HttpHeaders.AUTHORIZATION, credentials);
+			execute(request, HttpStatus.CREATED);
+
+			/*
+			 * verify
+			 */
+			final var createdUser = userMapper.selectByExample(new UserExample() {
+				{
+					createCriteria().andEmailEqualTo(user.getEmail());
+				}
+			}).stream().findFirst();
+			assertThat(createdUser.isPresent()).isTrue();
+			assertThat(createdUser.get()) //
+				.extracting(User::getEmail, User::getFirstName, User::getLastName, User::getAdmissionYear) //
+				.containsExactly( //
+					requestBody.getEmail(), requestBody.getFirstName(), requestBody.getLastName(), requestBody.getAdmissionYear());
+			assertThat(passwordEncoder.matches(requestBody.getPassword(), createdUser.get().getPassword())).isTrue();
+		}
+
+		@Test
+		void 異_メールアドレスが既に存在する() throws Exception {
+			/*
+			 * given
+			 */
+			final var loginUser = createLoginUser();
+			final var credentials = getLoginUserCredentials(loginUser);
+
+			final var user = UserSample.builder().password(LOGIN_USER_PASSWORD).build();
+			final var requestBody = modelMapper.map(user, UserCreateRequest.class);
+
+			// 同じメールアドレスのユーザを作成
+			userMapper.insert(user);
+
+			/*
+			 * test & verify
+			 */
+			final var request = postRequest(CREATE_USER_PATH, requestBody);
+			request.header(HttpHeaders.AUTHORIZATION, credentials);
+			execute(request, new ConflictException(ErrorCode.CONFLICT_EMAIL));
+		}
+
+		@ParameterizedTest
+		@MethodSource
+		void 有効なパスワードかチェック(final String password, final BaseException expectedException) throws Exception {
+			/*
+			 * given
+			 */
+			final var loginUser = createLoginUser();
+			final var credentials = getLoginUserCredentials(loginUser);
+
+			final var user = UserSample.builder().password(password).build();
+			final var requestBody = modelMapper.map(user, UserCreateRequest.class);
+
+			/*
+			 * test & verify
+			 */
+			final var request = postRequest(CREATE_USER_PATH, requestBody);
+			request.header(HttpHeaders.AUTHORIZATION, credentials);
+
+			if (Objects.isNull(expectedException)) {
+				execute(request, HttpStatus.CREATED);
+			} else {
+				execute(request, expectedException);
+			}
+		}
+
+		Stream<Arguments> 有効なパスワードかチェック() {
+			return Stream.of( // パスワード、期待される例外
+				// 有効
+				arguments("f4BabxEr", null), //
+				arguments("f4BabxEr4gNsjdtRpH9Pfs6Atth9bqdA", null), //
+				// 無効: 8文字以下
+				arguments("f4BabxE", new BadRequestException(ErrorCode.INVALID_PASSWORD_SIZE)), //
+				// 無効: 33文字以上
+				arguments("f4BabxEr4gNsjdtRpH9Pfs6Atth9bqdAN", new BadRequestException(ErrorCode.INVALID_PASSWORD_SIZE)), //
+				// 無効: 大文字を含まない
+				arguments("f4babxer", new BadRequestException(ErrorCode.TOO_SIMPLE_PASSWORD)), //
+				// 無効: 小文字を含まない
+				arguments("F4BABXER", new BadRequestException(ErrorCode.TOO_SIMPLE_PASSWORD)), //
+				// 無効: 数字を含まない
+				arguments("fxbabxEr", new BadRequestException(ErrorCode.TOO_SIMPLE_PASSWORD)) //
+			);
+		}
+
+		@Test
+		void 異_無効な認証ヘッダ() throws Exception {
+			/*
+			 * given
+			 */
+			final var user = UserSample.builder().build();
+			final var requestBody = modelMapper.map(user, UserCreateRequest.class);
+
+			/*
+			 * test & verify
+			 */
+			final var request = postRequest(CREATE_USER_PATH, requestBody);
 			request.header(HttpHeaders.AUTHORIZATION, "");
 			execute(request, new UnauthorizedException(ErrorCode.INVALID_ACCESS_TOKEN));
 		}
